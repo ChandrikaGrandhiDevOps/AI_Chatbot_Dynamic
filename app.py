2b6a4fecd0ff7c45108c155232996d5151c2a713
Chandrika <140220532+ChandrikaGrandhiDevOps@users.noreply.github.com>
2025-04-01 09:34:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+
 import streamlit as st
 import os
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
-###<<< i have to take this files from .env>>>###
-##os.environ["OPENAI_API_KEY"]="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"
-##Langmith tracking
-###os.environ["LANGCHAIN_TRACING_V2"]="true"
-##os.environ["LANGCHAIN_API_KEY"]="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"
+os.environ["OPENAI_API_KEY"]="open_ai"
+## Langmith tracking
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_API_KEY"]="lang_chain"
 
+## Prompt Template
 
 prompt=ChatPromptTemplate.from_messages(
     [
@@ -18,7 +19,10 @@ prompt=ChatPromptTemplate.from_messages(
         ("user","Question:{question}")
     ]
 )
-st.title('AI Chatbot')
+
+## streamlit framework
+
+st.title('Langchain Demo With OPENAI API')
 input_text=st.text_input("Search the topic u want")
 
 # openAI LLm
