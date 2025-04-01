8c35515e9d509f973fc3f901241eb6100e8001c9
Chandrika <140220532+ChandrikaGrandhiDevOps@users.noreply.github.com>
2025-04-01 10:16:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from langchain_openai import ChatOpenAI
+#from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_openai import ChatOpenAI
+#from langchain_openai import ChatOpenAI
 
 import streamlit as st
 import os
